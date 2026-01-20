fdd1f880bc19e18f653ff4546c090347be8091f1
longkey1 <longkey1@gmail.com>
2026-01-20 22:31:56 +0900

    chore: remove unused SearchResult model
    - Delete internal/models/search.go file containing SearchResult struct
    - The struct defined fields for paginated issue search results (StartAt, MaxResults, Total, Issues)

--- internal/models/search.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type SearchResult struct {
-	StartAt    int     `json:"startAt"`
-	MaxResults int     `json:"maxResults"`
-	Total      int     `json:"total"`
-	Issues     []Issue `json:"issues"`
-}
